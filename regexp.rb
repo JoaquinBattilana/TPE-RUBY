ad3dae656e1bbb56be2fd794cb4bade09725aa59
mlombardia <mlombardia@itba.edu.ar>
2018-04-01 19:39:01 -0300

    Add files via upload

--- regexp.rb
+++ regexp.rb
@@ -8,9 +8,20 @@ class RegExp
 	end
 
 	def lets_see(arg)
-		if (arg =~ /add(.*)/)
-			'Aca se agrega una tarea'
-		elsif (arg =~ /list/)
+		if (/add [a-zA-Z]+|\+/ =~ arg)
+			'entra aca'
+			if (/add \+[a-zA-Z]+ [a-zA-Z]+$/ =~ arg)
+				'tarea con grupo'
+			elsif(/add [a-zA-Z]+$/ =~ arg)
+				'tarea normal'
+			elsif (/add [a-zA-Z]+ due (tomorrow|today|[0-9][0-9]\/[0-9][0-9]\/[0-9][0-9]))$/ =~ arg)
+				'tarea con fecha'
+			elsif(/add \+[a-zA-Z]+ [a-zA-Z]+ due (tomorrow|today|[0-9][0-9]\/[0-9][0-9]\/[0-9][0-9]))$/ =~ arg)
+				'tarea con grupo y fecha'
+			else
+				'Comando no valido'
+			end
+		elsif (arg =~ /list [a-zA-Z]+/)
 			'Aca la estamos listando'
 		else
 			'Comando no valido'
@@ -20,11 +31,24 @@ end
 
 #ahora esta todo hardcodeado, pero la idea es que vean
 #como seria y despues implementamos el gets, etc.
-line1 = 'add tarea'
-line2 = 'list'
-line3 = 'afsdgarwtg'
+line1 = 'add'
+line2 = 'add asdasd'
+line3 = 'add asdfasdf due tomor'
+line4 = 'add +asdasd asfda due tomorrow'
+line5 = 'add '
+line6 = 'add +asd asdaf'
+line7 = 'add due tomorro'
+line8 = 'add +asdasd asfda due '
 
 check=RegExp.new				#OUTPUT
-puts check.lets_see(line1) # Aca se agrega una tarea
-puts check.lets_see(line2) # Aca la estamos listando
-puts check.lets_see(line3) # Comando no valido
+puts check.lets_see(line1) # 
+puts check.lets_see(line2) # 
+puts check.lets_see(line3) # 
+puts check.lets_see(line4) # 
+puts check.lets_see(line5) # 
+puts check.lets_see(line6) # 
+puts check.lets_see(line7) # 
+puts check.lets_see(line8) # 
+
+
+#add ([a-zA-Z]+)|((\+[a-zA-Z]+) [a-zA-Z]+)|([a-zA-Z]+ due (tomorrow|today|[0-9][0-9]\/[0-9][0-9]\/[0-9][0-9]))|((\+[a-zA-Z]+) [a-zA-Z]+ due (tomorrow|today|[0-9][0-9]\/[0-9][0-9]\/[0-9][0-9]))
