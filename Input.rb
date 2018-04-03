b3351c5025b110cf511c52f8d041e94974d48f78
mlombardia <mlombardia@itba.edu.ar>
2018-04-02 22:22:48 -0300

    Add files via upload

--- Input.rb
+++ Input.rb
@@ -1,62 +1,70 @@
 #include Input
 class Input
 	def initialize
-		@arg=nil
 	end
 
 	def to_s
 		'Error'
 	end
 
-	#def generate_desc(arg)
-	#	arg.delete(arg[0])
-	#	arg.delete(arg[0])
-	#	arg.delete(arg[-1])
-	#	arg.delete(arg[-1])
-	#	return arg[0] + ' ' + arg[1]
-	#end
+	def generate_desc(arg)
+		arg.delete(arg[0])
+		if (/\+[a-zA-Z]+$/ =~ arg[0])
+			arg.delete(arg[0])
+		end
+		sentence = ''
+		if (arg[-2] == 'due')
+			arg.delete(arg[-1])
+			arg.delete(arg[-1])
+		end
+		i=0
+		until arg[i] == nil
+			sentence += "#{i == 0? '': ' '}" + arg[i] 
+			i+=1
+		end
+		return sentence 
+	end
 
 	def input_check(holder,input)
 		if (/^add [a-zA-Z]+|\+/ =~ input)
 			if (/^add \+[a-zA-Z]+ ([a-zA-Z]+|\s)+$/ =~ input)
-				#@arg = input.split(/\s+/)
-				#holder.add(@arg[1],nil,self.generate_desc(@arg))
-			elsif(/^add ([a-zA-Z]+|\s)+$/ =~ input)
-				#@arg = input.split(/\s+/)
-				#holder.add(nil,nil,self.generate_desc(@arg))
+				arr = input.split(/\s+/)
+				holder.add(arg[2],nil,self.generate_desc(arr))
+			elsif(/^add [a-zA-Z]+$/ =~ input)
+				arr = input.split(/\s+/)
+				holder.add(nil,nil,self.generate_desc(arr))
 			elsif (/^add ([a-zA-Z]+|\s)+ due (tomorrow|today|[0-9][0-9]\/[0-9][0-9]\/[0-9][0-9])$/ =~ input)
-				#@arg = input.split(/\s+/)
-				#holder.add(nil, @arg[-1],self.generate_desc(@arg))
+				arr = input.split(/\s+/)
+				holder.add(nil, arg[-1],self.generate_desc(arr))
 			elsif(/^add \+[a-zA-Z]+ ([a-zA-Z]+|\s)+ due (tomorrow|today|[0-9][0-9]\/[0-9][0-9]\/[0-9][0-9])$/ =~ input)
-				#@arg = input.split(/\s+/)
-				#holder.add(@arg[1],@arg[-1], self.generate_desc(@arg)
+				arr = input.split(/\s+/)
+				holder.add(arr[1],arr[-1], self.generate_desc(arr))
 			else
 				'Comando no valido'
 			end
 		elsif (/^list/ =~ input)
 			if (/^list due (yesterday|today|tomorrow|this-week)/ =~ input)
-				#@arg = input.split(/\s+/)
-				#holder.list_due(@arg[2])
+				holder.list_due(arg[2])
 			elsif(/^list group/ =~ input)
-				#holder.list_group
+				holder.list_group
 			elsif (/^list overdue/ =~ input)
-				#holder.list_overdue
+				holder.list_overdue
 			elsif (/^list\s*$/ =~ input)
-				#holder.list
+				holder.list
 			else
 				'Comando no valido'
 			end		
 		elsif(/^ac\s*$/ =~ input)
-			'se archiva'
+			holder.ac
 		elsif(/^complete [0-9]+\s*$/ =~ input)
-			#@arg = input.split(/\s+/)
-			#holder.complete(@arg[1])
+			arr = input.split(/\s+/)
+			holder.complete(arg[1])
 		elsif(/^save [a-zA-Z]+\s*$/ =~ input)
-			#@arg = input.split(/\s+/)
-			#holder.savefile(@arg[1])
+			arr = input.split(/\s+/)
+			holder.savefile(arg[1])
 		elsif (/^open [a-zA-Z]+\s*$/ =~ input)
-			#@arg = input.split(/\s+/)
-			#holder.openfile(@arg[1])
+			arr = input.split(/\s+/)
+			holder.openfile(arg[1])
 		else	
 			'Comando no valido'
 		end
