99879fccff02026869e98ea690efe7c32c6dad88
JuanBaader <37674505+JuanBaader@users.noreply.github.com>
2018-05-10 16:56:55 -0300

    Add files via upload

--- StringDate.rb
+++ StringDate.rb
@@ -3,18 +3,23 @@ require "date"
 class StringDate
 	attr_reader :description,:date
 	
-	@@PREDEFINED_DATES={"yesterday"=>Date.today-1, "tomorrow"=>Date.today+1, "today"=>Date.today}
+	@@PREDEFINED_DATES={"yesterday"=>Date.today-1, "tomorrow"=>Date.today+1, "today"=>Date.today,"this_week"=>Date.today+7}
 	
-	def initialize(date)
-		@description=date
-		if(@@PREDEFINED_DATES.has_key?(date))
-			@date=@@PREDEFINED_DATES[date]
+	def initialize(string)
+		if(@@PREDEFINED_DATES.has_key?(string))
+			@date=@@PREDEFINED_DATES[string]
 		else
-			@date=Date.strptime(date, '%d/%m/%Y')
+			@date=Date.strptime(string, '%d/%m/%Y')
 		end
 	end
 	def to_s
-		@description
+		if (@date== Date.today)
+			return "today"
+		elsif (@date==(Date.today+1))
+			return "tomorrow"
+		else
+			return @date.day+"/"+@date.month+"/"+@date.year
+		end
 	end
 	def ==(other)
 		@date==other.date
