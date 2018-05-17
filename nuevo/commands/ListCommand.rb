d3989a0462da584eb7a57b965a6fcd8d7291b61b
JoaquinBattilana <jbattilana@itba.edu.ar>
2018-05-17 15:33:45 -0300

    Update ListCommand.rb

--- nuevo/commands/ListCommand.rb
+++ nuevo/commands/ListCommand.rb
@@ -10,7 +10,8 @@ class ListCommand
 	LIST_ALL_GROUPS = 4
 	LIST_OVERDUE = 5
 	LIST_ALL = 6
-	def initialize(parameters)
+	def initialize(holder, parameters)
+		@holder=holder
 		text=parameters.join(" ")
 		@date=nil
 		@group=nil
@@ -34,31 +35,30 @@ class ListCommand
 		end	
 	end
 
-	def execute(*objects)
-		holder=objects[0]
+	def execute()
 		case @command
 		when LIST_GROUP
 			puts "#{@group}"
-			holder.list_by_group(@group).each {|task| puts task.to_s_without_group}
+			@holder.list_by_group(@group).each {|task| puts task.to_s_without_group}
 		when LIST_BY_DATE
 			puts "All"
 			puts @date
-			puts holder.list_due(@date)
+			puts @holder.list_due(@date)
 		when LIST_WEEK
 			puts "All"
-			puts holder.list_due(@date)
+			puts @holder.list_due(@date)
 		when LIST_ALL_GROUPS
-			a=holder.list_group
-			a.each do |group|
+			aux=@holder.list_group
+			aux.each do |group|
 			puts group
-			(holder.list_by_group(group)).each {|task| puts task.to_s_without_group}
+			(@holder.list_by_group(group)).each {|task| puts task.to_s_without_group}
 			end
 		when LIST_OVERDUE
 			puts "All"
-			puts holder.list_overdue
+			puts @holder.list_overdue
 		when LIST_ALL
 			puts "All"
-			puts holder.list
+			puts @holder.list
 		end
 	end
 
