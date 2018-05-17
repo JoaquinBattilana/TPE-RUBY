59c8812ccfd790e06de7dfd24288b38a36c405ce
JoaquinBattilana <jbattilana@itba.edu.ar>
2018-05-17 15:30:02 -0300

    Update AcCommand.rb

--- nuevo/commands/AcCommand.rb
+++ nuevo/commands/AcCommand.rb
@@ -2,13 +2,13 @@ require_relative "Command.rb"
 
 class AcCommand
 	include Command
-	def initialize(params)
+	def initialize(holder, params)
 		raise NotSupportedCommand unless params.empty?
+		@holder = holder
 	end
 
-	def execute(*objects)
-		holder = objects[0]
-		holder.ac
+	def execute()
+		@holder.ac
 		puts "All completed task has been archived."
 	end
 end
