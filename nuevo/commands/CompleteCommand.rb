9bc6ca46c8bc363e22aca8501c3ea6750e1b7b10
JoaquinBattilana <jbattilana@itba.edu.ar>
2018-05-17 15:31:21 -0300

    Update CompleteCommand.rb

--- nuevo/commands/CompleteCommand.rb
+++ nuevo/commands/CompleteCommand.rb
@@ -2,16 +2,16 @@ require_relative "Command.rb"
 
 class CompleteCommand
 	include Command
-	def initialize(params)
+	def initialize(holder, params)
 		raise NotSupportedCommand if (params.size!=1 )
+		@holder=holder
 		@id=params[0].to_i
 	end
-	def execute(*objects)
-		holder=objects[0]
-		task = holder.find_task_by_id(@id)
+	def execute()
+		task = @holder.find_task_by_id(@id)
 		if task==nil
 			puts "Invalid Task"
-		elsif holder.complete(task)
+		elsif @holder.complete(task)
 			puts "Todo [#{@id}: #{task.description}] completed."
 		else
 			puts "Task #{@id} already completed"
